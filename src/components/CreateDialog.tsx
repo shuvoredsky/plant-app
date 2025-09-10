19216411381debfdc7d33a2d5b3759649c7dadfc
shuvoredsky <kumarshuvo265@gmail.com>
2025-09-10 12:11:19 +0600

    implement add plant button

---
+++ src/components/CreateDialog.tsx
@@ -0,0 +1,41 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { Sprout } from "lucide-react";
+
+<AlertDialog>
+  <AlertDialogTrigger asChild>
+    <Button
+      variant="default"
+      className="ml-auto flex items-center gap-2"
+      asChild
+    >
+      <span>
+        <Sprout className="w-4 h-4" />
+        Add Plant
+      </span>
+    </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>;
